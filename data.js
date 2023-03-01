6a1a297ed9f4feba39328c582e85312dfe94fa31
brunovonkoln <brunovonkoln@gmail.com>
2023-02-28 21:27:49 -0300

    ..

--- data.js
+++ data.js
@@ -5,5 +5,29 @@ export default  [
     cover: "alo.jpg",
     file: "alomundo.mp3",
   },
+  {
+    title:"2 - Mãos",
+    artist: "Zeca Pagodinho",
+    cover: "maos.jpg",
+    file: "maos.mp3",
+  },
+  {
+    title:"3 - Amor Estou Sofrendo / Que Nem Iôiô / Doce Inimigo",
+    artist: "Jorge Aragão",
+    cover: "sofre.jpg",
+    file: "sofre.mp3",
+  },
+  {
+    title:"4 - Um Lindo Sonho",
+    artist: "Fundo de Quintal",
+    cover: "sonho.jpg",
+    file: "sonho.mp3",
+  },
+  {
+    title:"5 - Se a fila andar",
+    artist: "Toninho Geraes",
+    cover: "fila.jfif",
+    file: "fila.mp3",
+  }
  ];
 
